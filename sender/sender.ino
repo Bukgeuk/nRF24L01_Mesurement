8b3b511dfe33b4ece08d53a795b7c38bea2d371e
공지운 <me@bukgeuk.dev>
2023-05-24 23:06:45 +0900

    command update & modify PALevel

--- sender/sender.ino
+++ sender/sender.ino
@@ -7,7 +7,7 @@ RF24 radio(7, 8);
 const uint8_t ADDRESS[][6] = {"00001", "00002"};
 const unsigned int TIMEOUT = 200; // ms
 const unsigned int TERM = 100; // ms
-const unsigned int COUNT = 100;
+const unsigned int DEFAULT_COUNT = 100;
 
 typedef struct _Data {
   unsigned long timestamp;
@@ -19,7 +19,7 @@ void setup() {
   Serial.begin(9600);
 
   radio.begin();
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setPALevel(RF24_PA_MAX);
 
   radio.openReadingPipe(1, ADDRESS[0]);
   radio.openWritingPipe(ADDRESS[1]);
@@ -33,46 +33,61 @@ unsigned int ackedSockets = 0;
 unsigned int totalLatency = 0;
 
 void loop() {
-  String command = Serial.readString();
-  if (strcmp(command.c_str(), "start\n") == 0) {
-    ackedSockets = 0;
-    totalLatency = 0;
+  String str = Serial.readString();
 
-    for (int i = 0; i < COUNT; i++) {
-      data.timestamp = millis();
-      radio.write(&data, sizeof(Data));
+  if (str.length() > 0) {
+    str.remove(str.length() - 1);
+    String command = "", argument = "";
+    short idx = str.indexOf(' ');
+    if (idx != -1) {
+      command = str.substring(0, idx);
+      argument = str.substring(idx + 1);
+    } else {
+      command = str;
+    }
+    
+    if (command.equalsIgnoreCase("start")) {
+      unsigned int count = DEFAULT_COUNT;
+      if (argument.length() > 0) count = argument.toInt();
+      ackedSockets = 0;
+      totalLatency = 0;
 
-      radio.startListening();
+      for (int i = 0; i < count; i++) {
+        data.timestamp = millis();
+        radio.write(&data, sizeof(Data));
 
-      unsigned long start = millis();
-      //bool timeout = true;
-      while (millis() < start + TIMEOUT) {
-        if (radio.available()) {
-          ackedSockets++;
-          radio.read(&data, sizeof(Data));
-          unsigned long end = millis();
-          totalLatency += end - data.timestamp;
-          break;
+        radio.startListening();
+
+        unsigned long start = millis();
+        //bool timeout = true;
+        while (millis() < start + TIMEOUT) {
+          if (radio.available()) {
+            ackedSockets++;
+            radio.read(&data, sizeof(Data));
+            unsigned long end = millis();
+            totalLatency += end - data.timestamp;
+            break;
+          }
         }
+
+        //if (timeout) Serial.println("timeout");
+
+        radio.stopListening();
+
+        char str[100] = "";
+        sprintf(str, "Progress: %d/%d", i+1, count);
+        Serial.println(str);
+
+        delay(TERM);
       }
 
-      //if (timeout) Serial.println("timeout");
-
-      radio.stopListening();
-
+      Serial.println("\n=============");
       char str[100] = "";
-      sprintf(str, "Progress: %d/%d", i+1, COUNT);
+      String acc = String((float)ackedSockets / count * 100);
+      String lat = String((float)totalLatency / ackedSockets);
+      sprintf(str, "Acc: %s%%, Avg Lat: %sms, Timeout: %d/%d", acc.c_str(), lat.c_str(), count - ackedSockets, count);
       Serial.println(str);
-
-      delay(TERM);
+      Serial.println("=============\n");
     }
-
-    Serial.println("\n=============");
-    char str[100] = "";
-    String acc = String((float)ackedSockets / COUNT * 100);
-    String lat = String((float)totalLatency / ackedSockets);
-    sprintf(str, "Acc: %s%%, Avg Lat: %sms, Timeout: %d/%d", acc.c_str(), lat.c_str(), COUNT - ackedSockets, COUNT);
-    Serial.println(str);
-    Serial.println("=============\n");
   }
 }
